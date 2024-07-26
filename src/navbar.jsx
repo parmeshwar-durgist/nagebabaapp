87ae9866ff67412afa38cf453fe10f0d9e7d7a16
Parmeshwar Durgist <150948536+parmeshwar-durgist@users.noreply.github.com>
2024-07-26 11:04:10 +0530

    Update navbar.jsx

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -9,7 +9,7 @@ function navbar()
                     <div className="container-fluid">
                         <div className="row" style={{boxSizing:'border-box'}}>
                             <div className="col-md-3 p-2 " style={{borderBottom:'1px solid black',borderTop:'1px solid black',boxSizing:'border-box'}}>
-                                <img src="./images/facebook.png" width={'30px'} style={{marginLeft:'20px'}} height={'30px'}></img>
+                                <img src={`${process.env.PUBLIC_URL}/images/facebook.png`} width={'30px'} style={{marginLeft:'20px'}} height={'30px'}></img>
                                 <img src="./images/instagram.png" width={'30px'} style={{marginLeft:'20px'}} height={'30px'}></img>
                                 <img src="./images/twitter.png" width={'30px'} style={{marginLeft:'20px'}} height={'30px'}></img>
                                 <img src="./images/whatsapp1.png" width={'30px'} style={{marginLeft:'20px'}} height={'30px'}></img>
